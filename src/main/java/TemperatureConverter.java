60637ef1d7d423a9882b6948fdf519917472be5b
vevego <veera.ruotsalainen@metropolia.fi>
2025-02-14 19:47:03 +0200

    completed

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,44 +1,30 @@
 
 public class TemperatureConverter {
 
-    public static int convertFahr(int f) {
+    public static int farenheitToCelcius(int f) {
         int c=  (int)((f-32) * 5.0/9.0);
         return c;
     }
 
     //This method convert thr cel to fsh
-    public static int convertCel(int c){
+    public static int celsiusToFarenheit(int c){
         return (int) ((c * 9.0 / 5.0) + 32);
     }
 
-    public static int kelvintoCel(int kel){
-        return (int) (kel-273.15);
-    }
-
-    public static  double	kelvinToFah(double kel) {
-        return Math.round((kel-273.15)* (9/5) + 32);
+    public static boolean isExtremeTemperature(int celsius) {
+        return celsius < -40 || celsius > 50;
     }
 
     public static void main(String [] args) {
 // Test convertFahr method
         int fahrenheit = 212;
-        int celsius = convertFahr(fahrenheit);
+        int celsius = farenheitToCelcius(fahrenheit);
         System.out.println(fahrenheit + " Fahrenheit is equal to " + celsius + " Celsius.");
 
         // Test convertCel method
         int cel = 100;
-        int fahr = convertCel(cel);
+        int fahr = celsiusToFarenheit(cel);
         System.out.println(cel + " Celsius is equal to " + fahr + " Fahrenheit.");
-
-        // Test kelvintoCel method
-        int kelvin = 273;
-        int celsiusFromKelvin = kelvintoCel(kelvin);
-        System.out.println(kelvin + " Kelvin is equal to " + celsiusFromKelvin + " Celsius.");
-
-
-        double kel = 300.1;
-        double far = kelvinToFah(kel);
-        System.out.println(kel + " Kelvin is equal to  " + kelvinToFah(kel) + " Fahrenheit.");
 
 
     }
--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -7,8 +7,8 @@ import static junit.framework.Assert.assertEquals;
 class TemperatureConverterTest {
     // initial method
     @Test
-    void convertFahr() {
-        int test = (TemperatureConverter.convertFahr(32));
+    void convertFahrenheit() {
+        int test = (TemperatureConverter.farenheitToCelcius(32));
 
         assertEquals(0, test);
 
@@ -17,21 +17,15 @@ class TemperatureConverterTest {
 
     // Add in the class demo
     @Test
-    void convertCel(){
-        int test= (TemperatureConverter.convertCel(20));
+    void convertCelsius(){
+        int test= (TemperatureConverter.celsiusToFarenheit(20));
         assertEquals(68,test);
     }
 
-    // Inclass exercise
-    @Test
-    void convertKel(){
-        int test= (TemperatureConverter.kelvintoCel(273));
-        assertEquals(0,test);
-    }
 
     @Test
-    void convertKeltoFarTest() {
-        double test =Math.round(TemperatureConverter.kelvinToFah(300.1));
-        assertEquals(59.0, test);
+    void isExtremeTemperature(){
+        boolean test= (TemperatureConverter.isExtremeTemperature(-45));
+        assertEquals(true,test);
     }
 }
