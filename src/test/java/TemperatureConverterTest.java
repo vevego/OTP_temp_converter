5e029cd19c59b07ff8082aac2d2d0b615084c3e1
vevego <veera.ruotsalainen@metropolia.fi>
2025-02-14 19:38:41 +0200

    test 123

--- src/test/java/TemperatureConverterTest.java
+++ src/test/java/TemperatureConverterTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static junit.framework.Assert.assertEquals;
 
-class TempConverterTest {
+class TemperatureConverterTest {
     // initial method
     @Test
     void convertFahr() {
